f549a423d81351b8dcd5b1df2d5a6bc7104b882d
Mitchelded <tretyakovanton0000@gmail.com>
2024-10-01 12:12:32 +0300

    test logic for listview
    test logic for listview amd create method GetInstanceView<T>

--- InventoryManagment/MainWindow.xaml.cs
+++ InventoryManagment/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿
 using InventoryManagment.Commands;
 using InventoryManagment.Models;
+using MaterialDesignThemes.Wpf;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,25 @@ namespace InventoryManagment
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		public ObservableCollection<Employees> employees = new ObservableCollection<Employees>()
+				{
+				new Employees()
+				{
+					IdEmployee = 1,
+									FirstName = "FirstName",
+									LastName = "LastName",
+									Position = "Position",
+									DepartmentID = 1,
+								},
+				new Employees()
+				{
+					IdEmployee = 2,
+									FirstName = "FirstName2",
+									LastName = "LastName2",
+									Position = "Position2",
+									DepartmentID = 2,
+								},
+				};
 		public MainWindow()
 		{
 			InitializeComponent();
@@ -33,10 +54,44 @@ namespace InventoryManagment
 				var q = EquipmentMethods.GetAllEquipments();
 
 
-				dg.ItemsSource = q.ToList();
+				ls.ItemsSource = employees;
+
+				//dg.ItemsSource = q.ToList();
 			}
 
 
+		}
+
+		private T GetInstanceView<T>(object sender) where T : class
+		{
+			var packIcon = sender as PackIcon;
+			if (packIcon != null)
+			{
+				var employeeEdit = packIcon.DataContext as T;
+				return employeeEdit; // Возвращаем DataContext, приведенный к T
+			}
+			return default; // Возвращаем значение по умолчанию для T, если packIcon null
+		}
+
+
+
+		private void packIconEdit_MouseDown(object sender, MouseButtonEventArgs e)
+		{
+			var employeeEdit = GetInstanceView<Employees>(sender);
+			var employeeToEdit = employees.Single(x => x.IdEmployee == employeeEdit.IdEmployee);
+
+			employeeToEdit.Position = employeeEdit.Position;
+			employeeToEdit.LastName = employeeEdit.LastName;
+			employeeToEdit.FirstName = employeeEdit.FirstName;
+			employeeToEdit.DepartmentID = employeeEdit.DepartmentID;
+			ls.Items.Refresh();
+		}
+
+		private void packIconRemove_MouseDown(object sender, MouseButtonEventArgs e)
+		{
+			var employeeToRemove = GetInstanceView<Employees>(sender);
+			employees.Remove(employeeToRemove);
+			ls.Items.Refresh();
 		}
 	}
 }

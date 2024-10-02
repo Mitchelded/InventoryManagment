854e5850c79e9a4ef9d79acb9faad2bf2d50e335
Mitchelded <tretyakovanton0000@gmail.com>
2024-10-02 12:55:40 +0300

    added interface for db methods

---
+++ InventoryManagment/Interface/IDbMethods.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using InventoryManagment.Models;
+
+namespace InventoryManagment.Interface
+{
+    public interface IDbMethods<T, TD>
+    {
+        void Add(T obj);
+        void AddMany(List<T> listObj);
+        void Remove(T obj);
+        IEnumerable<TD> GetAll();
+        void RemoveMany(List<T> listObj);
+        
+    }
+}
--- InventoryManagment/MainWindow.xaml.cs
+++ InventoryManagment/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
-﻿
-using InventoryManagment.Commands;
+﻿using InventoryManagment.Commands;
 using InventoryManagment.Models;
 using MaterialDesignThemes.Wpf;
 using System;
@@ -20,64 +19,65 @@ using System.Windows.Shapes;
 
 namespace InventoryManagment
 {
-	/// <summary>
-	/// Логика взаимодействия для MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window
-	{
-		public ObservableCollection<Employees> employees = new ObservableCollection<Employees>()
-				{
-				new Employees()
-				{
-					IdEmployee = 1,
-									FirstName = "FirstName",
-									LastName = "LastName",
-									Position = "Position",
-									DepartmentID = 1,
-								},
-				new Employees()
-				{
-					IdEmployee = 2,
-									FirstName = "FirstName2",
-									LastName = "LastName2",
-									Position = "Position2",
-									DepartmentID = 2,
-								},
-				};
-		public MainWindow()
-		{
-			InitializeComponent();
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Employees> employees = new ObservableCollection<Employees>()
+        {
+            new Employees()
+            {
+                IdEmployee = 1,
+                FirstName = "FirstName",
+                LastName = "LastName",
+                Position = "Position",
+                DepartmentID = 1,
+            },
+            new Employees()
+            {
+                IdEmployee = 2,
+                FirstName = "FirstName2",
+                LastName = "LastName2",
+                Position = "Position2",
+                DepartmentID = 2,
+            },
+        };
+
+        public MainWindow()
+        {
+            InitializeComponent();
 
 
-			using (InventoryManagmentEntities db = new InventoryManagmentEntities())
-			{
-				var q = EquipmentMethods.GetAllEquipments();
+            using (InventoryManagmentEntities db = new InventoryManagmentEntities())
+            {
+                EquipmentMethods equipmentMethods = new();
+                var q = equipmentMethods.GetAll();
 
 
-				ls.ItemsSource = employees;
+                // ls.ItemsSource = employees;
 
-				//dg.ItemsSource = q.ToList();
-			}
+                //dg.ItemsSource = q.ToList();
+            }
+        }
 
-
-		}
-		private void packIconEdit_MouseDown(object sender, MouseButtonEventArgs e)
-		{
-			var employeeEdit = GetInstancesView.GetInstanceView<Employees>(sender);
-			var employeeToEdit = employees.Single(x => x.IdEmployee == employeeEdit.IdEmployee);
-
-			employeeToEdit.Position = employeeEdit.Position;
-			employeeToEdit.LastName = employeeEdit.LastName;
-			employeeToEdit.FirstName = employeeEdit.FirstName;
-			employeeToEdit.DepartmentID = employeeEdit.DepartmentID;
-			ls.Items.Refresh();
-		}
-
-		private void packIconRemove_MouseDown(object sender, MouseButtonEventArgs e)
-		{
-			var employeeToRemove = GetInstancesView.GetInstanceView<Employees>(sender);
-			employees.Remove(employeeToRemove);
-			ls.Items.Refresh();
-		}
-	}
+        // private void packIconEdit_MouseDown(object sender, MouseButtonEventArgs e)
+        // {
+        //     var employeeEdit = GetInstancesView.GetInstanceView<Employees>(sender);
+        //     var employeeToEdit = employees.Single(x => x.IdEmployee == employeeEdit.IdEmployee);
+        //
+        //     employeeToEdit.Position = employeeEdit.Position;
+        //     employeeToEdit.LastName = employeeEdit.LastName;
+        //     employeeToEdit.FirstName = employeeEdit.FirstName;
+        //     employeeToEdit.DepartmentID = employeeEdit.DepartmentID;
+        //     ls.Items.Refresh();
+        // }
+        //
+        // private void packIconRemove_MouseDown(object sender, MouseButtonEventArgs e)
+        // {
+        //     var employeeToRemove = GetInstancesView.GetInstanceView<Employees>(sender);
+        //     employees.Remove(employeeToRemove);
+        //     ls.Items.Refresh();
+        // }
+    }
 }

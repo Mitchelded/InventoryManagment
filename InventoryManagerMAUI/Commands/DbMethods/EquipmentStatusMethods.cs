9a129ae6e19f0bc38f9f4289025f747e4a5a6204
aaaa <vvvv>
2024-10-07 12:59:12 +0300

    work update

---
+++ InventoryManagerMAUI/Commands/DbMethods/EquipmentStatusMethods.cs
@@ -0,0 +1,40 @@
+﻿using InventoryManagment.Interface;
+using InventoryManagment.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagerMAUI.Commands.DbMethods
+{
+	internal class EquipmentStatusMethods : IDbMethods<EquipmentStatus, EquipmentStatus>
+	{
+		public void Add(EquipmentStatus obj)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void AddMany(List<EquipmentStatus> listObj)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<EquipmentStatus> GetAll()
+		{
+			throw new NotImplementedException();
+		}
+
+		public async void Remove(EquipmentStatus obj)
+		{
+			using InventoryManagmentEntities db = new InventoryManagmentEntities();
+			db.EquipmentStatus.Remove(obj);
+			await db.SaveChangesAsync();
+		}
+
+		public void RemoveMany(List<EquipmentStatus> listObj)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
--- InventoryManagerMAUI/ViewModels/EquipmentStatusViewModel.cs
+++ InventoryManagerMAUI/ViewModels/EquipmentStatusViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.ObjectModel;
 using System.Windows.Input;
+using InventoryManagerMAUI.Commands.DbMethods;
 using InventoryManagment.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,65 +8,83 @@ namespace InventoryManagerMAUI.ViewModels;
 
 public class EquipmentStatusViewModel : ViewModelBase
 {
-    public ObservableCollection<EquipmentStatus> EquipmentStatus { get; set; }
-	public ICommand DeleteCommand { get; }
-    private readonly InventoryManagmentEntities db;
-	public EquipmentStatusViewModel()
-    {
-        db = new();
-        db.Database.EnsureCreated();
-        db.EquipmentStatus.Load();
-        EquipmentStatus = db.EquipmentStatus.Local.ToObservableCollection();
-        DeleteCommand = new Command<EquipmentStatus>(OnDelete);
+	public ObservableCollection<EquipmentStatus> EquipmentStatus { get; set; }
+
+	private EquipmentStatus _selectedStatus;
+
+	public EquipmentStatus SelectedStatus
+	{
+		get => _selectedStatus;
+		set { _selectedStatus = value; OnPropertyChanged(nameof(SelectedStatus));}
+
 	}
 
+	public ICommand DeleteCommand { get; }
+	public ICommand UpdateCommand { get; }
 
-
-	private async void OnDelete(EquipmentStatus status)
+	private readonly InventoryManagmentEntities db;
+	public EquipmentStatusViewModel()
 	{
-        if(status != null) {
-			db.EquipmentStatus.Remove(status);
-			await db.SaveChangesAsync();
+		db = new();
+		db.Database.EnsureCreated();
+		db.EquipmentStatus.Load();
+		EquipmentStatus = db.EquipmentStatus.Local.ToObservableCollection();
+		DeleteCommand = new Command<EquipmentStatus>(OnDelete);
+		UpdateCommand = new Command<EquipmentStatus>(OnUpdate);
+	}
+
+	private void OnUpdate(EquipmentStatus status)
+	{
+		if (status != null)
+		{
+			var q = db.EquipmentStatus.Find(status.IdStatus);
+			status = q;
+			db.SaveChanges();
 		}
+	}
 
-
+	private void OnDelete(EquipmentStatus status)
+	{
+		if (status != null)
+		{
+			EquipmentStatusMethods methods = new();
+			methods.Remove(status);
+		}
 	}
 
 	private int _idStatus;
-    private string _description;
-    private string _name;
-	private EquipmentStatus selectedStatus;
-
+	private string _description;
+	private string _name;
 	public int IdStatus
-    {
-        get => _idStatus;
-        set
-        {
-            if (value == _idStatus) return;
-            _idStatus = value;
-            OnPropertyChanged(nameof(IdStatus));
-        }
-    }
+	{
+		get => _idStatus;
+		set
+		{
+			if (value == _idStatus) return;
+			_idStatus = value;
+			OnPropertyChanged(nameof(IdStatus));
+		}
+	}
 
-    public string Name
-    {
-        get => _name;
-        set
-        {
-            if (value == _name) return;
-            _name = value;
-            OnPropertyChanged(nameof(Name));
-        }
-    }
+	public string Name
+	{
+		get => _name;
+		set
+		{
+			if (value == _name) return;
+			_name = value;
+			OnPropertyChanged(nameof(Name));
+		}
+	}
 
-    public string Description
-    {
-        get => _description;
-        set
-        {
-            if (value == _description) return;
-            _description = value;
-            OnPropertyChanged(nameof(Description));
-        }
-    }
+	public string Description
+	{
+		get => _description;
+		set
+		{
+			if (value == _description) return;
+			_description = value;
+			OnPropertyChanged(nameof(Description));
+		}
+	}
 }

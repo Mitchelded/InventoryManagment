58ecea2b174484ff17ec30a035aa5644e1cf7b83
Mitchelded <tretyakovanton0000@gmail.com>
2024-12-12 18:56:08 +0300

    Delete ViewModels and Views

---
+++ InventoryManagerMAUI/View/OrdersManagementView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagerMAUI.View;
+
+public partial class OrdersManagementView : ContentPage
+{
+    public OrdersManagementView()
+    {
+        InitializeComponent();
+    }
+}
